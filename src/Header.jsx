cbde79101eb4dfbfbed592d90d9753d22e45d451
Interstellarr-Guy <interstellarguy29@gmail.com>
2026-03-17 16:27:19 +0530

    months UI

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,25 @@ import './App.css'
 
 function Header() {
   return (
-    <div className="bg-[#121212] text-white p-4 text-5xl font-bold ">
-      2026
+    <div className="navbar bg-[#121212] text-white p-4  font-bold ">
+         
+         <div className='flex flex-row justify-around'>
+          <h1 className='head text-5xl'>2026</h1>
+              <h4 id="mon">Jan</h4>
+              <h4 id="mon">Feb</h4>
+              <h4 id="mon">Mar</h4>
+              <h4 id="mon">Apr</h4>
+              <h4 id="mon">May</h4>
+              <h4 id="mon">Jun</h4>
+              <h4 id="mon">Jul</h4>
+              <h4 id="mon"> Aug</h4>
+              <h4 id="mon">Sep</h4>
+              <h4 id="mon">Oct</h4>
+              <h4 id="mon">Nov</h4>
+              <h4 id="mon">Dec</h4>
+
+         </div>
+         
     </div>
     
   )
