b2facdca0a3c9a1f2bebb4bfee286a05d3af3124
Interstellarr-Guy <interstellarguy29@gmail.com>
2026-03-18 17:59:35 +0530

    dates added

--- src/Dummy.jsx
+++ src/Dummy.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { Content } from './components/Content'
 import { Route, Routes } from 'react-router-dom'
 import { OpenMsg } from './components/OpenMsg'
+import { Jan } from './components/Months/Jan'
+import { Feb } from './components/Months/Feb'
+import { Mar } from './components/Months/Mar'
+import { Apr } from './components/Months/Apr'
+import { May } from './components/Months/May'
+import { Jun } from './components/Months/Jun'
+import { Jul } from './components/Months/Jul'
+import { Aug } from './components/Months/Aug'
+import { Sep } from './components/Months/Sep'
+import { Oct } from './components/Months/Oct'
+import { Nov } from './components/Months/Nov'
+import { Dec } from './components/Months/Dec'
 
 
 
@@ -10,7 +22,21 @@ function Dummy()  {
     <div className='bg-[#888] h-[90%] w-full '>
       <Routes>
         <Route path='/' element={<OpenMsg/>}></Route>
-       <Route path='/jan' element={<Content/>}></Route>  
+       
+       <Route path='/jan' element={<Jan/>}></Route>
+       <Route path='/feb' element={<Feb/>}></Route>
+       <Route path='/mar' element={<Mar/>}></Route>
+       <Route path='/apr' element={<Apr/>}></Route>
+       <Route path='/may' element={<May/>}></Route>
+       <Route path='/jun' element={<Jun/>}></Route>
+       <Route path='/jul' element={<Jul/>}></Route>
+       <Route path='/aug' element={<Aug/>}></Route>
+       <Route path='/sep' element={<Sep/>}></Route>
+       <Route path='/oct' element={<Oct/>}></Route>
+       <Route path='/nov' element={<Nov/>}></Route>
+       <Route path='/dec' element={<Dec/>}></Route>
+       
+
       </Routes>      
     </div>
   )
---
+++ src/components/Months/Jun.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export const Jun = () => {
+  return (
+    <div className='bg-[#888] h-full w-full grid grid-cols-[repeat(7,1fr)] grid-rows-[repeat(5,1fr)] border border-gray-500'>
+        
+          <div className='box border-gray-400'><textarea id="con"></textarea></div>
+          
+          <div className='box border-gray-400'>
+          <textarea placeholder='1 
+        Type to start' id="con"></textarea></div>
+
+    <div className='box border-gray-400'><textarea placeholder='2' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='3' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='4' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='5' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='6' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='7' id="con"></textarea></div>  
+    <div className='box border-gray-400'><textarea placeholder='8' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='9' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='10' id="con"></textarea></div>
+
+    <div className='box border-gray-400'><textarea placeholder='11' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='12' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='13' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='14' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='15' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='16' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='17' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='18' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='19' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='20' id="con"></textarea></div>
+    
+    <div className='box border-gray-400'><textarea placeholder='21' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='22' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='23' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='24' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='25' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='26' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='27' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='28' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='29' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='30' id="con"></textarea></div>
+    <div className='box border-gray-400'><textarea placeholder='31' id="con"></textarea></div>
+    </div>
+  )
+}
